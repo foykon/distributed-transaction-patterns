4dc6f51bacff47fe5cd631ba6c3af5d8c608a2f3
foykon <foykon9@gmail.com>
2025-11-17 22:20:33 +0300

    stcokapi sending and publish messages now

--- saga-orchestration/Shared/RabbitMQSettings.cs
+++ saga-orchestration/Shared/RabbitMQSettings.cs
@@ -9,5 +9,7 @@ namespace Shared
     public static class RabbitMQSettings
     {
         public const string Stcok_OrderCreatedEvent = "stock-order-created-event";
+        public const string Order_StockNotReservedEvent = "order-stock-not-reserved-event";
+        public const string Payment_StockReservedEvent = "payment-stock-reserved-event";
     }
 }
--- saga-orchestration/Stock.API/Consumers/OrderCreatedEventConsumer.cs
+++ saga-orchestration/Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -1,5 +1,6 @@
 ﻿using MassTransit;
 using MongoDB.Driver;
+using Shared;
 using Shared.Events;
 using Stock.API.Models;
 using Stock.API.Services;
@@ -9,10 +10,14 @@ namespace Stock.API.Consumers
     public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
     {
         private readonly MongoDBService _mongoDBService;
+        private readonly ISendEndpointProvider _sendEndpoint;
+        private readonly IPublishEndpoint _publishEndponit;
 
-        public OrderCreatedEventConsumer(MongoDBService mongoDBService)
+        public OrderCreatedEventConsumer(MongoDBService mongoDBService, ISendEndpointProvider sendEndpoint, IPublishEndpoint publishEndponit)
         {
             _mongoDBService = mongoDBService;
+            _sendEndpoint = sendEndpoint;
+            _publishEndponit = publishEndponit;
         }
 
         public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
@@ -33,12 +38,32 @@ namespace Stock.API.Consumers
                     stock.Quantity -= orderItem.Quantity;
                     await collection.FindOneAndReplaceAsync(s=>s.ProductId == orderItem.ProductId, stock);
                 }
-                // call payment
+                ISendEndpoint sendEndpoint = await _sendEndpoint.GetSendEndpoint(new Uri($"queue: {RabbitMQSettings.Payment_StockReservedEvent}"));
+                await sendEndpoint.Send(new StockReservedEvent()
+                {            
+                    BuyerId = context.Message.BuyerId,
+                    OrderId = context.Message.OrderId,
+                    TotalPrice = context.Message.TotalPrice,
+                    OrderItemMessages = context.Message.OrderItemMessages
+                });
             }
             else
             {
-                // stock failed
-                //call order cancel event
+                List<Guid> nonStcokProductIds = new List<Guid>();
+                for (int i =0; i< stockResult.Count; i++)
+                {
+                    if(stockResult[i] == false)
+                    {
+                        nonStcokProductIds.Add(context.Message.OrderItemMessages[i].ProductId);
+                    }
+                }
+
+                await _publishEndponit.Publish(new StockNotReservedEvent()
+                {
+                    BuyerId = context.Message.BuyerId,
+                    OrderId = context.Message.OrderId,
+                    Message = nonStcokProductIds.Count > 0 ? $"Stock not available for products: {string.Join(", ", nonStcokProductIds)}" : "Stock not available"
+                });
             }
         }
     }
