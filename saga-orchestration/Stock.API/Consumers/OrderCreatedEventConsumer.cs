c529179aca080699b18881d30d649aaf452c38c2
foykon <foykon9@gmail.com>
2025-11-17 21:38:45 +0300

    stcokApı designed as an stcok api

---
+++ saga-orchestration/Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -0,0 +1,40 @@
+﻿using MassTransit;
+using MongoDB.Driver;
+using Shared.Events;
+using Stock.API.Models;
+using Stock.API.Services;
+
+namespace Stock.API.Consumers
+{
+    public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
+    {
+        private readonly MongoDBService _mongoDBService;
+
+        public OrderCreatedEventConsumer(MongoDBService mongoDBService)
+        {
+            _mongoDBService = mongoDBService;
+        }
+
+        public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            List<bool> stockResult = new List<bool>();
+
+            IMongoCollection<Models.Stock> collection =  _mongoDBService.GetCollection<Models.Stock>();
+            foreach(var orderItem in context.Message.OrderItemMessages)
+            {
+                stockResult.Add( await (await collection.FindAsync(s => s.ProductId == orderItem.ProductId && s.Quantity >= orderItem.Quantity)).AnyAsync());
+            }    
+
+            if(stockResult.TrueForAll(s => s == true))
+            {
+                // update stock
+                // call payment
+            }
+            else
+            {
+                // stock failed
+                //call order cancel event
+            }
+        }
+    }
+}
