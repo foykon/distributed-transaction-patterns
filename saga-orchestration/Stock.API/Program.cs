233374ff7a240e49a48cbe64b773a3b62072caa7
foykon <foykon9@gmail.com>
2025-11-23 18:36:35 +0300

    stcok api connecttes to db and mq

--- saga-orchestration/Stock.API/Program.cs
+++ saga-orchestration/Stock.API/Program.cs
@@ -1,4 +1,6 @@
 using MassTransit;
+using Stock.API.Services;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,28 @@ builder.Services.AddMassTransit(x =>
     });
 });
 
+builder.Services.AddSingleton<MongoDbService>();
+
+
 var app = builder.Build();
+
+
+using var scope = app.Services.CreateScope();
+
+var mongoDb = scope.ServiceProvider.GetRequiredService<Stock.API.Services.MongoDbService>();
+
+var stockCollection = mongoDb.GetCollection<Stock.API.Models.Stock>();
+
+if (!stockCollection.FindSync(s => true).Any())
+{
+    await stockCollection.InsertManyAsync(new List<Stock.API.Models.Stock>()
+    {
+        new Stock.API.Models.Stock(){ ProductId = 1, Quantity=100 },
+        new Stock.API.Models.Stock(){ ProductId = 2, Quantity=150 },
+        new Stock.API.Models.Stock(){ ProductId = 3, Quantity=200 },
+        new Stock.API.Models.Stock(){ ProductId = 4, Quantity=250 },
+        new Stock.API.Models.Stock(){ ProductId = 5, Quantity=300 },
+    });
+}
 
 app.Run();
---
+++ saga-orchestration/Stock.API/Services/MongoDbService.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+
+namespace Stock.API.Services
+{
+    public class MongoDbService
+    {
+        readonly IMongoDatabase _database;
+        public MongoDbService(IConfiguration configuration)
+        {
+            MongoClient client = new MongoClient(configuration.GetConnectionString("MongoDB"));
+            _database = client.GetDatabase("StockDB");
+        }
+
+        public IMongoCollection<T> GetCollection<T>()
+        {
+            return _database.GetCollection<T>(typeof(T).Name.ToLowerInvariant());
+        }
+    }
+}
